e157c0dfd43398e61554be2d220d26c6d0e7ba74
Sayam Ajmal <itssanesayam@gmail.com>
2022-04-26 13:32:09 +0100

    setup done

---
+++ src/utils/index.js
@@ -0,0 +1,24 @@
+const fs = require("fs")
+const Module = require("module")
+
+const addMovie = (movieObj) => {
+    try {
+        const stringObj = JSON.stringify(movieObj)
+        fs.writeFileSync('./storage.json', stringObj)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+const listMovies = () => {
+    try{
+        const jsonList = fs.readFileSync('./storage.json')
+        const listParser = JSON.parse(jsonList)
+        console.log(listParser)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = {addMovie, listMovies}
